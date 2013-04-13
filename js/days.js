e52561084504302d0108e74df9530f0405cc4cb7
Ron Waldon <jokeyrhyme@gmail.com>
2013-04-13 10:00:22 +1000

    use AngularJS to do the heavy lifting

--- js/days.js
+++ js/days.js
@@ -1,33 +1,35 @@
-function displayTime() {
+function CounterCtrl($scope) {
     var left = new Date(2013, 02, 15, 17);
-    var now = new Date();
-    var difference = now.getTime() - left.getTime();
 
-    var days = difference / (1000 * 60 * 60 * 24);
-    var hours = (difference / (1000 * 60 * 60)) % 24;
-    var minutes = (difference / (1000 * 60)) % 60;
-    var seconds = (difference / 1000) % 60;
+    timer = window.setInterval(function() {
+        var now = new Date();
+        var difference = now.getTime() - left.getTime();
 
-    document.getElementById('days').innerText = Math.round(days);
-    document.getElementById('hours').innerText = Math.round(hours);
-    document.getElementById('minutes').innerText = Math.round(minutes);
-    document.getElementById('seconds').innerText = Math.round(seconds);
+        var days = difference / (1000 * 60 * 60 * 24);
+        var hours = (difference / (1000 * 60 * 60)) % 24;
+        var minutes = (difference / (1000 * 60)) % 60;
+        var seconds = (difference / 1000) % 60;
+
+        $scope.days = Math.round(days);
+        $scope.hours = Math.round(hours);
+        $scope.minutes = Math.round(minutes);
+        $scope.seconds = Math.round(seconds);
+
+        $scope.$apply();
+    }, 1000);
 }
 
-function epitaph() {
+function EpitaphCtrl($scope) {
     var options = ["PHP deity",
         "conversationalist",
         "skateboarder",
         "surfer",
         '"experimenter"'];
-    document.getElementById('epitaph').innerText =
-        "Since our beloved " +
-        options[Math.floor(Math.random() * options.length)] +
-        " left us";
+
+    $scope.noun = options[Math.floor(Math.random() * options.length)];
 }
 
-function story() {
-    var days = Math.floor((new Date().getTime() - new Date(2013, 02, 15, 17).getTime()) / (1000 * 60 * 60 * 24));
+function StoryCtrl($scope) {
     var options1 = [
         "alcohol rehabilitation program students",
         "kibble park dealers",
@@ -42,16 +44,9 @@ function story() {
         "flooded the car park",
         "set the curtains on fire",
         "lost a laptop with all the server passwords on it"
-        ];
-    document.getElementById('story').innerText = "Day " +
-        days +
-        " in a post BTB society: Today the " +
-        options1[Math.floor(Math.random() * options1.length)] + ' ' +
-        options2[Math.floor(Math.random() * options2.length)];
-}
+    ];
 
-document.addEventListener('DOMContentLoaded', function() {
-    timer = window.setInterval(displayTime, 1000);
-    epitaph();
-    story();
-});
+    $scope.days = Math.floor((new Date().getTime() - new Date(2013, 02, 15, 17).getTime()) / (1000 * 60 * 60 * 24));
+    $scope.subject = options1[Math.floor(Math.random() * options1.length)];
+    $scope.action = options2[Math.floor(Math.random() * options2.length)];
+}
